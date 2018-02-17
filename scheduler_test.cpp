62871b521de8374bba5b1b22294ffb910d27170e
losfair <zhy20000919@hotmail.com>
2018-02-17 21:49:46 +0800

    scheduler test

--- scheduler_test.cpp
+++ scheduler_test.cpp
@@ -3,11 +3,46 @@
 #include "scheduler.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
+#include <condition_variable>
+#include <unistd.h>
+
+// https://stackoverflow.com/questions/4792449/c0x-has-no-semaphores-how-to-synchronize-threads
+class semaphore
+{
+private:
+    std::mutex mutex_;
+    std::condition_variable condition_;
+    unsigned long count_ = 0; // Initialized as locked.
+
+public:
+    void notify() {
+        std::unique_lock<decltype(mutex_)> lock(mutex_);
+        ++count_;
+        condition_.notify_one();
+    }
+
+    void wait() {
+        std::unique_lock<decltype(mutex_)> lock(mutex_);
+        while(!count_) // Handle spurious wake-ups.
+            condition_.wait(lock);
+        --count_;
+    }
+
+    bool try_wait() {
+        std::unique_lock<decltype(mutex_)> lock(mutex_);
+        if(count_) {
+            --count_;
+            return true;
+        }
+        return false;
+    }
+};
 
 std::atomic<int> exec_count(0);
 
 void chk_exec_count() {
-    if(exec_count.load() == 5) {
+    if(exec_count.load() == 7) {
         exit(0);
     }
 }
@@ -54,6 +89,54 @@ void fib_n(struct coroutine *crt) {
     chk_exec_count();
 }
 
+void _async_target(struct coroutine *crt, void *user_data) {
+    int *v = (int *) user_data;
+    (*v)++;
+    coroutine_async_exit(crt);
+}
+
+void async_calls(struct coroutine *crt) {
+    int v = 0;
+    for(int i = 0; i < 100000; i++) {
+        coroutine_async_enter(crt, _async_target, (void *) &v);
+    }
+    printf("async_calls: v = %d\n", v);
+    exec_count.fetch_add(1);
+    chk_exec_count();
+}
+
+struct nested_context {
+    std::atomic<int> count;
+    semaphore notify;
+    int target_value;
+};
+
+void _nested_co_inner(struct coroutine *crt) {
+    nested_context *ctx = (nested_context *) crt -> user_data;
+    int current = ctx -> count.fetch_add(1);
+    if(current + 1 > ctx -> target_value) {
+        abort();
+    }
+    if(current + 1 == ctx -> target_value) {
+        ctx -> notify.notify();
+    }
+}
+
+void nested_co(struct coroutine *crt) {
+    nested_context ctx;
+    ctx.target_value = 10000;
+
+    for(int i = 0; i < ctx.target_value; i++) {
+        start_coroutine(crt -> pool, 4096, _nested_co_inner, (void *) &ctx);
+    }
+
+    ctx.notify.wait();
+
+    printf("nested_co OK\n");
+    exec_count.fetch_add(1);
+    chk_exec_count();
+}
+
 int main() {
     struct task_pool pool;
     task_pool_init(&pool, 1);
@@ -63,8 +146,10 @@ int main() {
     start_coroutine(&pool, 4096, fib_n, (void *) 37);
     start_coroutine(&pool, 4096, fib_n, (void *) 32);
     start_coroutine(&pool, 4096, fib_n, (void *) 27);
+    start_coroutine(&pool, 4096, async_calls, NULL);
+    start_coroutine(&pool, 4096, nested_co, NULL);
 
-    for(int i = 0; i < 3; i++) {
+    for(int i = 0; i < 4; i++) {
         std::thread([&pool]() {
             struct scheduler sch;
             scheduler_init(&sch, &pool);
@@ -72,12 +157,9 @@ int main() {
         }).detach();
     }
 
-    struct scheduler sch;
-    scheduler_init(&sch, &pool);
-    scheduler_run(&sch);
-
-    scheduler_destroy(&sch);
-    task_pool_destroy(&pool);
+    while(1) {
+        sleep(3600);
+    }
 
     return 0;
 }
