f91d4fa06a479c5084686581ca40959078fbe744
losfair <zhy20000919@hotmail.com>
2018-02-19 20:34:32 +0800

    Update panic example

--- examples/panic/src/main.rs
+++ examples/panic/src/main.rs
@@ -1,5 +1,8 @@
 extern crate coroutines;
+use std::sync::atomic::{Ordering, AtomicUsize, ATOMIC_USIZE_INIT};
 use std::panic::catch_unwind;
+
+static PANIC_COUNT: AtomicUsize = ATOMIC_USIZE_INIT;
 
 fn panic_thread() {
     fn inner_fn() {
@@ -8,9 +11,18 @@ fn panic_thread() {
 
     let ret = catch_unwind(|| inner_fn());
     assert!(ret.is_err());
+
+    let id = PANIC_COUNT.fetch_add(1, Ordering::SeqCst);
+    println!("OK {}", id);
+
+    let id = PANIC_COUNT.load(Ordering::SeqCst);
+    if id == 200000 {
+        std::process::exit(0);
+    }
 }
 
 fn main() {
+    std::panic::set_hook(Box::new(|_| {}));
     for _ in 0..200000 {
         coroutines::spawn(panic_thread);
     }
