1538fc122d56f5c1b20825eefbffbe3791e999c0
losfair <zhy20000919@hotmail.com>
2018-02-20 13:13:25 +0800

    Add CI scripts

---
+++ ci_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm /usr/bin/clang || true
+rm /usr/bin/clang++ || true
+ln -s $(which clang-5.0) /usr/bin/clang
+ln -s $(which clang++-5.0) /usr/bin/clang++
+
+cd core_impl
+make || exit 1
+cp libunblock_hook.so /usr/lib/ || exit 1
+cd ..
+
+cargo test --release || exit 1
