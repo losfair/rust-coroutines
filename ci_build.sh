b72aadb09db8521ca85cf41647959a682d4a51ae
losfair <zhy20000919@hotmail.com>
2018-02-20 13:35:20 +0800

    Remove sudo requirement

--- ci_build.sh
+++ ci_build.sh
@@ -5,7 +5,9 @@ export CXX=/usr/local/clang-5.0.0/bin/clang++-5.0
 
 cd core_impl
 make unblock_hook || exit 1
-cp libunblock_hook.so /usr/lib/ || exit 1
 cd ..
+
+export LD_LIBRARY_PATH="./core_impl/"
+export RUSTFLAGS="-L./core_impl/"
 
 cargo test --release || exit 1
