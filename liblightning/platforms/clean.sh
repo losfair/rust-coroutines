91f0f29eee04719134f47c02065fb599dfa46056
losfair <zhy20000919@hotmail.com>
2018-03-04 18:50:48 +0800

    Revert "liblightning!"
    This reverts commit 58258407ef95fd3640a21b4c729760ad7f02604c.
    
    Move liblightning out as a separate crate.

--- liblightning/platforms/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd $(dirname $0)
-
-find . -name "*.o" -exec rm '{}' ';'
-find . -name "*.a" -exec rm '{}' ';'
--- liblightning/src/lib.rs
+++
@@ -1,7 +0,0 @@
-extern crate libc;
-#[macro_use]
-extern crate lazy_static;
-
-pub mod co;
-mod platform;
-
